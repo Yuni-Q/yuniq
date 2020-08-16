60f7c111b71296597b53bf5b2e163a4f0c53e1da
Yuni-Q <carnotcycle2@naver.com>
2020-08-16 07:45:51 +0000

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-6c0cbf20fa3a9732cd27.js"
+    "url": "webpack-runtime-92e5901632a2e8a25270.js"
   },
   {
     "url": "styles.2edb9c47f168420dfc0a.css"
@@ -45,7 +45,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "93df7d449565dee78abae8f047a9e49e"
+    "revision": "6197dff09ac69f9198b01137b34f8869"
   },
   {
     "url": "polyfill-d98ce29e75983b5711b7.js"
