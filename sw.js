9d4cca63c7b01c69df0530daf549a2fb935b488c
Yuni-Q <lyh6425@gmail.com>
2024-02-25 03:18:18 +0000

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-19c2660737f54e7f5491.js"
+    "url": "webpack-runtime-3ac5a37c4138a90636ca.js"
   },
   {
     "url": "styles.833d29091dec255371fb.css"
@@ -49,7 +49,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "e0634f413e75f922b4aba1aad03e4af4"
+    "revision": "9ca55308916ed41ab9fec893c50678d1"
   },
   {
     "url": "manifest.webmanifest",
